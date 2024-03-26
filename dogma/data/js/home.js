9312e3b18f9dea6534a3839b4dfc243c25c7ec18
Nikuzo <41453940+NikuzoKO@users.noreply.github.com>
2024-03-26 13:19:36 -0300

    Dogma

---
+++ dogma/data/js/home.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+	var menu = document.getElementById('startMenu');
+	menu.addEventListener('keypress', function(e) {
+		if (e.key === 'Enter') {
+			if (menu.value == 'Jugar') {
+				window.open('data/pages/dogmaPage1.html', '_self');
+			} else if (menu.value == 'Opciones') {
+				window.open('data/pages/dogmaOptions.html', '_self');
+			} else if (menu.value == 'Contacto') {
+				window.open('data/pages/dogmaContact.html', '_self');
+			} else if (menu.value == 'Créditos') {
+				window.open('data/pages/dogmaCredits.html', '_self');
+			} else {
+				window.open('#', '_self')
+			}
+		}
+	});
+
+});
